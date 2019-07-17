3cdc2bfeb5aac2e3ea23f46addc47ba16c0554e6
Ingmar Sell <ingmarsell1@gmail.com>
2019-07-17 15:17:49 -0400

    Add mesa-utils

--- build.sh
+++ build.sh
@@ -1,4 +1,6 @@
 #!/bin/bash
+sudo echo "Installing mesa-utils"
+sudo apt install mesa-utils -y
 echo "Downloading driver"
 version="$(glxinfo | grep "OpenGL version string" | rev | cut -d" " -f1 | rev)"
 wget http://us.download.nvidia.com/XFree86/Linux-x86_64/"$version"/NVIDIA-Linux-x86_64-"$version".run
